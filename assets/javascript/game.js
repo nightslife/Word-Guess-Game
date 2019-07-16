5a868ca5cb2cd7975f750e0dfbdaecca5d5e96c6
nightslife <ragefail1212@gmail.com>
2019-07-15 21:18:21 -0400

    Incorrect guesses counts down

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,11 @@
 var choices = ["ALIEN", "DRACULA", "FRANKENSTEIN", "GODZILLA", "JAWS", "MARTIANS", "MUMMY", "PREDATOR", "WEREWOLF", "ZOMBIE", "KING KONG", "THE THING"];
 var round = choices[Math.floor((Math.random() * 10))];
 var spaces = "_ ";
+var wins = 0;
+var numberGuesses = 8;
+var guessesLeft = document.getElementById("guessesLeft")
+
+
 
 console.log("The answer is: " + round);
 console.log("It has " + round.length + " letters");
@@ -15,4 +20,16 @@ var roundLetters = Array.from(round);
 console.log(roundLetters[3]);
 
 
+// Determine Key Press
+document.onkeypress = function (event) {
+    var letterPress = event.key.toUpperCase();
+    console.log(letterPress);
+    // Compare Key Press to Letters
+    if (roundLetters.indexOf(letterPress) === -1) {
+        numberGuesses--;
+        console.log(numberGuesses);
+        guessesLeft.textContent = numberGuesses
+
+    }
+};
 
