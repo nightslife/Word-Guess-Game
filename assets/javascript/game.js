9b1aae72cfe2e9dd3c7b325b17529faf058209aa
nightslife <ragefail1212@gmail.com>
2019-07-16 02:21:32 -0400

    hangman all letters will fill. all incorrect guesses put in bank.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,58 @@
 // Initial variables/choices
 var choices = ["ALIEN", "DRACULA", "FRANKENSTEIN", "GODZILLA", "JAWS", "MARTIANS", "MUMMY", "PREDATOR", "WEREWOLF", "ZOMBIE", "KING KONG", "THE THING"];
-var round = choices[Math.floor((Math.random() * 10))];
-var spaces = "_ ";
+var round = choices[Math.floor((Math.random() * 12))];
+var spaces = "_";
 var wins = 0;
 var numberGuesses = 8;
-var guessesLeft = document.getElementById("guessesLeft")
+var guessesLeft = document.getElementById("guessesLeft");
+var currentGuesses = [];
+var incorrect = document.getElementById("incorrect");
+var allBlanksArray = Array.from(spaces.repeat(round.length));
+var roundLetters = Array.from(round);
+console.log(roundLetters);
+console.log(allBlanksArray);
 
 
 
 console.log("The answer is: " + round);
-console.log("It has " + round.length + " letters");
+// console.log("It has " + round.length + " letters");
 
 // Sets the number of spaces on the html
+console.log(roundLetters);
+roundLetters.forEach(function (space) {
+    if(space === " ") {
+        console.log(roundLetters.indexOf(space));
+        allBlanksArray[roundLetters.indexOf(space)] = " ";
+    }
+    return allBlanksArray
+});
+
+console.log(allBlanksArray)
 var hidden = document.getElementById("blanks");
-hidden.textContent = spaces.repeat(round.length);
+hidden.textContent = allBlanksArray.join("");
 
 // Makes an array of the letter answers
-var roundLetters = Array.from(round);
-console.log(roundLetters[3]);
+
+// console.log(roundLetters[3]);
+
 
 
 // Determine Key Press
 document.onkeypress = function (event) {
     var letterPress = event.key.toUpperCase();
-    console.log(letterPress);
+    // console.log(letterPress);
     // Compare Key Press to Letters
-    if (roundLetters.indexOf(letterPress) === -1) {
+    if (roundLetters.indexOf(letterPress) < 0 & currentGuesses.indexOf(letterPress) < 0) {
         numberGuesses--;
-        console.log(numberGuesses);
-        guessesLeft.textContent = numberGuesses
-
-    }
+        // console.log(numberGuesses);
+        guessesLeft.textContent = numberGuesses;
+        currentGuesses.push(letterPress);
+        incorrect.textContent = currentGuesses.join(", ");
+      }
+    else for(i=0; i<roundLetters.length; i++){
+            if (roundLetters[i] === letterPress){
+                allBlanksArray[i] = letterPress
+                hidden.textContent = allBlanksArray.join("");
+            }
+        }   
 };
-
